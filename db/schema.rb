ccdda2b1790ee85197ed1355fec0afef4a5c2fc3
chonasan <tk24120824@gmail.com>
2021-02-03 14:53:50 +0000

    Bookers2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_164610) do
+ActiveRecord::Schema.define(version: 2021_02_03_121106) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "title"
+    t.text "opinion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +26,22 @@ ActiveRecord::Schema.define(version: 2021_01_31_164610) do
     t.text "opinion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.text "introduction"
+    t.string "image"
+    t.string "image_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
