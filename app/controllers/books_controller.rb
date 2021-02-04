80e2cac163f4afd51e29e9ccf214f0d1e7ef7144
chonasan <tk24120824@gmail.com>
2021-02-04 06:46:07 +0000

    Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
   
   def show
+    @book = Book.new
+    @book = Book.find(params[:id])
   end
   
   def index
+     @book = Book.new
+     @books = Book.all
   end
   
 end
