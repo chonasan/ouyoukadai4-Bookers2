9d492b6eee05524dc3132e6178318455a1778816
chonasan <tk24120824@gmail.com>
2021-02-11 05:50:20 +0000

    Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,24 @@
 class BooksController < ApplicationController
-  
+
   def show
     @user = current_user
     @book = Book.new
     @books = Book.all
   end
-  
+
   def index
     @user = current_user
     @book = Book.new
     @books = Book.all
   end
-  
+
   # 　@tweet = Tweet.new(tweet_params)  # フォームから送られてきたデータ(body)をストロングパラメータを経由して@tweetに代入
   #   @tweet.user_id = current_user.id # user_idの情報はフォームからはきていないので、deviseのメソッドを使って「ログインしている自分のid」を代入
   #   @tweet.save
   #   redirect_to tweets_path
-  
+
   def create
+      @user = current_user
       @book = Book.new(book_params)
       @book.user_id = current_user.id
       @books = Book.all
@@ -28,11 +29,11 @@ class BooksController < ApplicationController
       render action: :index
      end
   end
-   
+
 end
-  
+
    private
     def book_params
       params.require(:book).permit(:title, :body, :profile_image)
     end
-   
+
